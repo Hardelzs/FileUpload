d58ac5f664b45a3d2bbcad7d9cf4c6f99e4def6b
Hardelz <135560555+Hardelzs@users.noreply.github.com>
2024-07-24 22:08:06 +0100

    restructure index.js

--- js/index.js
+++ js/index.js
@@ -1,21 +1,20 @@
-const buttonUp = document.getElementById("btn")
-const realLife = document.getElementById("real-file")
-const textfile = document.getElementById("text-file")
-const upload = document.getElementById("btn2")
+const buttonUp = document.getElementById("btn");
+const realLife = document.getElementById("real-file");
+const textfile = document.getElementById("text-file");
+const upload = document.getElementById("btn2");
 
 buttonUp.addEventListener("click", () => {
-    realLife.click();   
+  realLife.click();
 });
 
 realLife.addEventListener("change", () => {
-    if(realLife.value){
-        textfile.innerHTML = realLife.value.match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1];
-    }else{
-        textfile.innerHTML = "No file chosen, yet"
-    }
-
+  if (realLife.value) {
+    textfile.innerHTML = realLife.value.match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1];
+  } else {
+    textfile.innerHTML = "No file chosen, yet";
+  }
 });
 
 upload.addEventListener("click", () => {
-    document.getElementById("dev").innerHTML = textfile.innerHTML
-})
+  document.getElementById("dev").innerHTML = textfile.innerHTML;
+});
